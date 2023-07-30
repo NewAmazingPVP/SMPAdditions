f99fec00beab185fd3ca3757216d067956f1e1b4
NewAmazingPVP <gamingofpat@gmail.com>
2023-07-29 23:14:11 -0400

    auto restart

---
+++ src/main/java/mc/smpadditions/CombatLog.java
@@ -0,0 +1,24 @@
+package mc.smpadditions;
+
+import com.github.sirblobman.combatlogx.api.ICombatLogX;
+import com.github.sirblobman.combatlogx.api.manager.IDeathManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static mc.smpadditions.Additions.additions;
+
+public class CombatLog {
+
+
+}
